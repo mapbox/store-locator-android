3ce0df1629df291c08df752cce64eed6032278ed
Langston Smith <langston.smith@mapbox.com>
2018-03-05 07:23:01 -0800

    removed directions api call onMarkerClick()

--- StoreLocator/app/src/main/java/com/mapbox/storelocator/activity/MapActivity.java
+++ StoreLocator/app/src/main/java/com/mapbox/storelocator/activity/MapActivity.java
@@ -400,17 +400,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             }
           }
           adjustMarkerSelectStateIcons(marker);
-
-          // Get the directionsApiClient route to the selected marker except if the mock device location marker is selected
-          if (!marker.getIcon().equals(customThemeManager.getMockLocationIcon())) {
-            // Check for an internet connection before making the call to Mapbox Directions API
-            if (deviceHasInternetConnection()) {
-              // Start the call to the Mapbox Directions API
-              getInformationFromDirectionsApi(marker.getPosition(), false, null);
-            } else {
-              Toast.makeText(MapActivity.this, R.string.no_internet_message, Toast.LENGTH_SHORT).show();
-            }
-          }
         }
 
         // Return true so that the selected marker's info window doesn't pop up
